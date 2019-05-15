73ebecf608cf5e2bc08f8425727f492b8372a5dc
Juan 'Dientuki' Farias <dientuki@gmail.com>
2019-05-15 10:24:03 -0300

    try to add validators

---
+++ test/localStorage-cache-api.test.js
@@ -0,0 +1,11 @@
+import LocalStorage  from '../src/localStorage-cache-api';  // our module
+
+describe('LocalStorage cache api', () => {
+
+  let LS;
+
+  beforeEach(() => {
+    LS = new LocalStorage();
+  });
+
+});
